8587befef07f45353b6eb6df4e57bcfabf3d29f3
Emily Ann Smith <emsmith@uchicago.edu>
2022-08-31 07:33:18 -0500

    Update setup script for OS build

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,9 +4,14 @@ cd xilinx_bitbake
 curl https://storage.googleapis.com/git-repo-downloads/repo > repo && chmod a+x repo
 export PATH=$PATH:$PWD
 
-repo init -u git://github.com/Xilinx/yocto-manifests.git -b rel-v2020.2 && repo sync &&  repo start rel-2020.2 --all
+repo init -u https://github.com/Xilinx/yocto-manifests.git -b rel-v2020.2 && repo sync &&  repo start rel-2020.2 --all
 
 cd sources
+#git clone https://github.com/kratsg/meta-l1calo.git
 git clone git@github.com:kratsg/meta-l1calo.git
+cd ../
 source setupsdk && bitbake-layers add-layer ../sources/meta-l1calo 
+
+printf "\nPARALLEL_MAKE = \"-j 24\"\n" >> conf/local.conf
+printf "\nBB_NUMBER_THREADS = \"24\"\n" >> conf/local.conf
 #manually set machine in local.conf and add parallelizability 
