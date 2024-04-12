aeae7bdfded6928aba43014300ebf9894af58484
Kristin Dona <donakristin@gmail.com>
2024-04-12 15:00:00 +0200

    add log output file in case of crashes

--- recipes-core/init/init-ipmc-auto-shutdown/run-init-ipmc-auto-shutdown.sh
+++ recipes-core/init/init-ipmc-auto-shutdown/run-init-ipmc-auto-shutdown.sh
@@ -1,1 +1,1 @@
-python3 /software/misc/ipmc_auto_shutdown/gpio_all_sensor_autoshutdown.py &
+python3 -u /software/misc/ipmc_auto_shutdown/gpio_all_sensor_autoshutdown.py >> /logs/ipmc-update.log 2>&1 &
